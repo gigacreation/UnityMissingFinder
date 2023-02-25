f84fec677dc87e2c20630394e972c5e077286c92
Gigacee <gigacee@gigacreation.jp>
2023-02-25 20:10:06 +0900

    Delete unnecessary ReSharper comment

--- Assets/UnityMissingFinder/Editor/MissingFinderMenuItems.cs
+++ Assets/UnityMissingFinder/Editor/MissingFinderMenuItems.cs
@@ -1,6 +1,4 @@
-﻿// ReSharper disable RedundantUsingDirective
-
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEditor;
